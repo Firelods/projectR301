cc421a3eb6bd6a42651097f3f991e520c2cbe139
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2022-12-26 14:43:58 +0100

    v1 du css du brandComponent

--- src/app/interfaces/brand.ts
+++ src/app/interfaces/brand.ts
@@ -3,12 +3,14 @@ export class Brand {
   title: string;
   link: string;
   imageURL: string
+  description: string;
 
-  constructor(id: number, title: string, link: string, imageURL: string){
+  constructor(id: number, title: string, link: string, imageURL: string, description:string){
     this.id = id;
     this.title = title;
     this.link = link;
     this.imageURL = imageURL;
+    this.description = description;
   }
 }
 
