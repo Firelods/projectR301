a071b415d4317343f6bc551e7e8a4b76b49fe45e
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-01-18 00:08:59 +0100

    Ajouter marque et product fonctionnel dans AdminComponent #11

--- src/app/admin-page/brand-gestion/brand-gestion.component.ts
+++ src/app/admin-page/brand-gestion/brand-gestion.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Brand } from 'src/app/interfaces/brand';
+import { Product } from 'src/app/interfaces/product';
+import { BrandService } from 'src/app/service/brand.service';
 
 @Component({
   selector: 'app-brand-gestion',
   templateUrl: './brand-gestion.component.html',
-  styleUrls: ['./brand-gestion.component.css']
+  styleUrls: ['./brand-gestion.component.css'],
 })
 export class BrandGestionComponent {
+  makeSearch: boolean = false;
+  addBrand: boolean = false;
+  listBrand: Brand[] = [];
+  addBrandForm: FormGroup;
+  constructor(private brandService: BrandService) {
+    this.addBrandForm = new FormGroup({
+      title: new FormControl(''),
+      description: new FormControl(''),
+      imageurl: new FormControl(''),
+      link: new FormControl(''),
+    });
+  }
 
+  searchBrand() {
+    var search = (<HTMLInputElement>document.getElementById("search")).value;
+    console.log(search);
+
+    this.brandService.searchBrand(search).subscribe((data: Brand[]) => {
+      this.listBrand = data;
+      this.makeSearch = true;
+    });
+  }
+
+  submitForm() {
+    console.log(this.addBrandForm.value.imageURL)
+    this.brandService.addBrand(new Brand(-1, this.addBrandForm.value.title, this.addBrandForm.value.link, this.addBrandForm.value.imageurl, this.addBrandForm.value.description)).subscribe((data: Brand) => {
+      this.listBrand.push(data);
+    });
+  }
 }
--- src/app/service/brand.service.ts
+++ src/app/service/brand.service.ts
@@ -8,13 +8,28 @@ import { RequestServiceService } from './request.service';
   providedIn: 'root'
 })
 export class BrandService {
+  searchBrand(search: string) {
+    let param: HttpParams = new HttpParams();
+    param = param.append('search', search);
+    return this.request.get<Brand[]>(this.requestService.url + 'searchBrand', { params: param }).pipe(map((data: Brand[]) => {
+      return data;
+    }));
+  }
 
   constructor(private requestService: RequestServiceService, private request: HttpClient) { }
-  getBrand(brandName:string){
-    let param:HttpParams = new HttpParams();
+  getBrand(brandName: string) {
+    let param: HttpParams = new HttpParams();
     param = param.append('brand', brandName);
-    return this.request.get<Brand[]>(this.requestService.url+'getBrand',{params: param}).pipe(map((data: Brand[]) => {
+    return this.request.get<Brand[]>(this.requestService.url + 'getBrand', { params: param }).pipe(map((data: Brand[]) => {
       return data[0];
     }));
+  }
+  addBrand(brand: Brand) {
+    const formData = new FormData();
+    formData.append('title', brand.title);
+    formData.append('link', brand.link);
+    formData.append('imageURL', brand.imageURL);
+    formData.append('description', brand.description);
+    return this.request.post<Brand>(this.requestService.url + 'addBrand', formData);
   }
 }
